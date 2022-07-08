5329c9f8916086b547ace3f777512b5dbab8ab0f
toojoseph <toojoseph38@gmail.com>
2022-07-08 14:08:32 -0400

    create a clone for the slack home page

---
+++ images/Slack is your digital HQ _ Slack_files/geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "NAIROBI",
+      'continent': "AF",
+      'country': "KE",
+      'region': "",
+      'dma': ""
+    },
+    'ip':"154.159.88.70"
+  }]);
+})
+//
+()
+
+;
